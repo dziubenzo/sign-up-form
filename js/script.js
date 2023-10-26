40ee1c1a351a2c8bd513843e17ee04c2ab6fb577
dziubenzo <dziubenzo@gmail.com>
2023-10-26 11:47:58 +0200

    Work on checking whether passwords match in JS

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ const confirmPasswordField = document.querySelector(
   'input[name="confirm-password"]'
 );
 const formWrapper = document.querySelector('.form-wrapper');
+const messagePara = document.querySelector('.password-message');
 
 formWrapper.addEventListener('keyup', (event) => {
-  console.log(event.target.value);
+  if (event.target === passwordField && passwordField.value) {
+    messagePara.textContent = 'Password must have at least 8 characters';
+  }
 });
