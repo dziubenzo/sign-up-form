cb8260b2a796ad64f24fabdc2d9c63ad58cb69f4
dziubenzo <dziubenzo@gmail.com>
2023-10-26 12:10:37 +0200

    Validate whether password match with JS

--- js/script.js
+++ js/script.js
@@ -6,7 +6,24 @@ const formWrapper = document.querySelector('.form-wrapper');
 const messagePara = document.querySelector('.password-message');
 
 formWrapper.addEventListener('keyup', (event) => {
-  if (event.target === passwordField && passwordField.value) {
+  if (
+    passwordField.value.length < 8 &&
+    (passwordField.value || confirmPasswordField.value)
+  ) {
+    passwordField.classList.remove('wrong-passwords');
+    confirmPasswordField.classList.remove('wrong-passwords');
     messagePara.textContent = 'Password must have at least 8 characters';
+  } else if (
+    passwordField.value != confirmPasswordField.value &&
+    passwordField.value.length >= 8 &&
+    confirmPasswordField.value.length >= 8
+  ) {
+    passwordField.classList.add('wrong-passwords');
+    confirmPasswordField.classList.add('wrong-passwords');
+    messagePara.textContent = 'Passwords do not match';
+  } else {
+    passwordField.classList.remove('wrong-passwords');
+    confirmPasswordField.classList.remove('wrong-passwords');
+    messagePara.textContent = '';
   }
 });
